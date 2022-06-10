82b97ecf8b226d02236f08c14bc3f0a27fe1b5a5
eefou <79306234+Xeonisius@users.noreply.github.com>
2022-06-10 10:16:02 +0300

    Update DM-4.py

--- DM-4.py
+++ DM-4.py
@@ -1,24 +1,24 @@
 from random import random
 from math import sqrt
 
-#Матрица игры
-matrix = [[2, -3], [-1, 2]]
+#Таблица игры
+field = [[2, -3], [-1, 2]]
 
 #Генератор случайных чисел
-def rand(r):
+def rand(p):
     i = random()
-    if i <= r:
+    if i <= p:
         d = 0
     else:
         d = 1
     return d
 
-#подсчёт данных
-def maths(res, pA, pB, dev, games):
+#Подсчет и вывод данных
+def calcul(ans, pA, pB, dev, exp):
     print('Вероятность выбора первой строки А:', pA)
     print('Вероятность выбора первого столбца B:', pB)
-    print('Выиграш А:', res[0], 'Выигрыш В:', res[1])
-    aver = res[0]/games
+    print('Выиграш А:', ans[0], 'Выигрыш В:', ans[1])
+    aver = ans[0]/exp
     print('Средний выиграш А за одну игру:', aver)
     expected = 8*pA*pB - 5*pA - 3*pB + 2
     print('Математическое ожидание:', expected)
@@ -30,113 +30,112 @@ def maths(res, pA, pB, dev, games):
 
 
 # Симуляция обычной игры
-def game(pA, pB):
-    #количество игр в симуляции
-    games = 100
-    #для первых двух симуляций
+def exp(pA, pB):
+    #количество игр
+    exp = 100
     if (type(pA) == float or type(pA) == int) and (type(pB) == int or type(pB) == float):
-        res = [0, 0]
-        res_mat = []
-        for i in range(games):
-            n = matrix[rand(pA)][rand(pB)]
-            res_mat.append(n)
-            res = [res[0]+n, res[1]-n]
-        aver = res[0]/games
+        ans = [0, 0]
+        ans_mat = []
+        for i in range(exp):
+            n = field[rand(pA)][rand(pB)]
+            ans_mat.append(n)
+            ans = [ans[0]+n, ans[1]-n]
+        aver = ans[0]/exp
         dev = 0
-        for i in res_mat:
+        for i in ans_mat:
             dev += ((i - aver))**2
-        dev = sqrt(dev/(games-1))
-        maths(res, pA, pB, dev, games)
+        dev = sqrt(dev/(exp-1))
+        calcul(ans, pA, pB, dev, exp)
 
 
 # Симуляция игры с подкреплением для игрока А
-def game_reinA(pA, pB):
-    games = 100
+def exp_reinA(pA, pB):
+    exp = 100
     if pA == 'reinforcement' and (type(pB) == int or type(pB) == float):
-        res = [0, 0]
-        res_mat = []
-        box = [10, 10]
-        pA = box[0]/(box[0]+box[1])
-        for i in range(games):
+        ans = [0, 0]
+        ans_mat = []
+        sqr = [10, 10]
+        pA = sqr[0]/(sqr[0]+sqr[1])
+        for i in range(exp):
             randomise = rand(pA)
-            n = matrix[randomise][rand(pB)]
+            n = field[randomise][rand(pB)]
             if n > 0:
-                box[randomise] += 2
-            pA = box[0]/(box[0]+box[1])
-            res_mat.append(n)
-            res = [res[0]+n, res[1]-n]
-        aver = res[0]/games
+                sqr[randomise] += 2
+            pA = sqr[0]/(sqr[0]+sqr[1])
+            ans_mat.append(n)
+            ans = [ans[0]+n, ans[1]-n]
+        aver = ans[0]/exp
         dev = 0
-        for i in res_mat:
+        for i in ans_mat:
             dev += ((i - aver))**2
-        dev = sqrt(dev/(games-1))
-        maths(res, pA, pB, dev, games)
+        dev = sqrt(dev/(exp-1))
+        calcul(ans, pA, pB, dev, exp)
 
 
 # Симуляция с наказанием для игрока А
-def game_punishnA(pA, pB):
-    games = 100
+def exp_punishnA(pA, pB):
+    exp = 100
     if pA == 'punishment' and (type(pB) == int or type(pB) == float):
-        res = [0, 0]
-        res_mat = []
-        box = [100, 100]
-        pA = box[0] / (box[0] + box[1])
-        for i in range(games):
+        ans = [0, 0]
+        ans_mat = []
+        sqr = [100, 100]
+        pA = sqr[0] / (sqr[0] + sqr[1])
+        for i in range(exp):
             randomise = rand(pA)
-            n = matrix[randomise][rand(pB)]
+            n = field[randomise][rand(pB)]
             if n < 0:
-                box[randomise] += n
-            pA = box[0] / (box[0] + box[1])
-            res_mat.append(n)
-            res = [res[0] + n, res[1] - n]
-        aver = res[0] / games
+                sqr[randomise] += n
+            pA = sqr[0] / (sqr[0] + sqr[1])
+            ans_mat.append(n)
+            ans = [ans[0] + n, ans[1] - n]
+        aver = ans[0] / exp
         dev = 0
-        for i in res_mat:
+        for i in ans_mat:
             dev += ((i - aver)) ** 2
-        dev = sqrt(dev / (games-1))
-        maths(res, pA, pB, dev,  games)
+        dev = sqrt(dev / (exp-1))
+        calcul(ans, pA, pB, dev,  exp)
 
 # Симуляция с подкреплением для обоих игроков
-def game_reinAB(pA, pB):
-    games = 100
+def exp_reinAB(pA, pB):
+    exp = 100
     if pA == 'reinforcement' and pB == 'reinforcement':
-        res = [0, 0]
-        res_mat = []
-        boxA = [10, 10]
-        boxB = [10, 10]
-        pA = boxA[0] / (boxA[0] + boxA[1])
-        pB = boxB[0] / (boxB[0] + boxB[1])
-        for i in range(games):
+        ans = [0, 0]
+        ans_mat = []
+        sqrA = [10, 10]
+        sqrB = [10, 10]
+        pA = sqrA[0] / (sqrA[0] + sqrA[1])
+        pB = sqrB[0] / (sqrB[0] + sqrB[1])
+        for i in range(exp):
             randomiseA = rand(pA)
             randomiseB = rand(pB)
-            n = matrix[randomiseA][randomiseB]
+            n = field[randomiseA][randomiseB]
             if n < 0:
-                boxB[randomiseB] -= n
+                sqrB[randomiseB] -= n
             else:
-                boxA[randomiseA] += n
-            pA = boxA[0] / (boxA[0] + boxA[1])
-            pB = boxB[0] / (boxB[0] + boxB[1])
-            res_mat.append(n)
-            res = [res[0] + n, res[1] - n]
-        aver = res[0] / games
+                sqrA[randomiseA] += n
+            pA = sqrA[0] / (sqrA[0] + sqrA[1])
+            pB = sqrB[0] / (sqrB[0] + sqrB[1])
+            ans_mat.append(n)
+            ans = [ans[0] + n, ans[1] - n]
+        aver = ans[0] / exp
         dev = 0
-        for i in res_mat:
+        for i in ans_mat:
             dev += ((i - aver)) ** 2
-        dev = sqrt(dev / (games-1))
-        maths(res, pA, pB, dev, games)
+        dev = sqrt(dev / (exp-1))
+        calcul(ans, pA, pB, dev, exp)
 
 
 
 
 
 
-print('Оба игрока равновероятно (с вероятностью 0.5) выбирают одну или другую строку/столбец')
-game(0.5, 0.5)
-print('Игрок A равновероятно выбирает строку, а игрок B (случайно) выбирает красный столбец втрое реже, чем синий.')
-game(0.5, 0.25)
-print('Обучение с подкреплением игрока A')
-game_reinA('reinforcement', 0.25)
-print('Обучение с наказанием игрока A')
-game_punishnA('punishment', 0.25)
-print('Оба игрока обучаются с подкреплением')
-game_reinAB('reinforcement', 'reinforcement')
+print('Игроки равновероятно выбирают одну или другую строку/столбец')
+exp(0.5, 0.5)
+print('Игрок A равновероятно выбирает строку, а игрок B выбирает красный столбец втрое реже, чем синий.')
+exp(0.5, 0.25)
+print('Игрок А с подкреплением')
+exp_reinA('reinforcement', 0.25)
+print('Игрок А с наказанием')
+exp_punishnA('punishment', 0.25)
+print('Оба игрока с подкреплением')
+exp_reinAB('reinforcement', 'reinforcement')
